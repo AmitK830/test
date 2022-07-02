a7afbd90f7985cf290879bc4b43a95fa54ba3ff8
AmitK830 <amitsit24@gmail.com>
2022-07-02 16:43:50 +0530

    Check the function

--- new_notebook.py
+++ new_notebook.py
@@ -1,1 +1,13 @@
 print("This is the new text notebook for Github")
+
+
+def subzero(x):
+    if x < -10:
+        print("Freezing Point")
+    elif x >= -10:
+        print("No idea")
+
+    else:
+        print("New to do lots of experiment")
+
+subzero(-30)
