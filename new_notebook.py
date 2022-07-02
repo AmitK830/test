b443ef5e9c00b49a042fff1a097ea0bd0a421903
AmitK830 <amitsit24@gmail.com>
2022-07-02 16:58:26 +0530

    Pressure Function added to repo

--- new_notebook.py
+++ new_notebook.py
@@ -6,8 +6,16 @@ def subzero(x):
         print("Freezing Point")
     elif x >= -10:
         print("No idea")
-
+    elif x > 0:
+        print("Need to check th data")
     else:
         print("New to do lots of experiment")
 
-subzero(-30)
+
+def pressure(x):
+    if x > 1000:
+        print("Atmospheric pressure")
+    elif x < 0:
+        print("Vacuum Pressure")
+    else:
+        print("Use Barometer or other device to measure")
